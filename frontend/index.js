253aaf71cfc1b6da7cfb90023cf91ad847b99ee8
SalixSauce <107958386+SalixSauce@users.noreply.github.com>
2025-02-03 16:38:50 +0000

    reconfigured for vercel

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ import './styles/reset.css'
 import './styles/styles.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from '../components/App'
+import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
 
 const domNode = document.getElementById('root')
--- webpack.config.js
+++ webpack.config.js
@@ -1,116 +1,38 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
-const DEVELOPMENT = 'development'
-const ENV = process.env.NODE_ENV || DEVELOPMENT
-const IS_DEV = ENV === DEVELOPMENT
-
-const HTML_LOADER = 'html-loader'
-const CSS_LOADER = 'css-loader'
-const BABEL_LOADER = 'babel-loader'
-const STRING_REPLACE_LOADER = 'string-replace-loader'
-const FILE_LOADER = 'file-loader'
-
-const SERVER_URL = /http:\/\/localhost:9009/g
-const FRONTEND_PORT = 3003
-
-const DIST_PATH = 'dist'
-const PUBLIC_PATH = IS_DEV ? '/' : './'
-const INDEX_HTML_PATH = './frontend/index.html'
-const INDEX_JS_PATH = './frontend/index.js'
-const AUDIO_PATH = 'audio/'
-
-const JS_NAME = IS_DEV ? 'index.js' : 'index.[contenthash].js'
-const CSS_NAME = IS_DEV ? 'style.css' : 'style.[contenthash].css'
-const MP3_NAME = '[name].[ext]'
-
-const SOURCE_MAP = IS_DEV ? 'source-map' : false
-
-const config = {
-  entry: INDEX_JS_PATH,
-  mode: ENV,
+module.exports = {
+  entry: './frontend/index.js', // Explicitly point to frontend/index.js
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/'
   },
-  devtool: SOURCE_MAP,
   plugins: [
     new HtmlWebpackPlugin({
-      template: INDEX_HTML_PATH,
-    }),
-    new MiniCssExtractPlugin({
-      filename: CSS_NAME,
-    }),
+      template: './frontend/index.html', // Use correct path to HTML
+    })
   ],
   devServer: {
-    static: path.join(__dirname, DIST_PATH),
-    historyApiFallback: true,
-    compress: true,
-    port: FRONTEND_PORT,
-    client: { logging: 'none' },
-    proxy: [
-      {
-        context: ['/api'],
-        target: 'http://localhost:9009',
-        changeOrigin: true,
-        secure: false,
-      },
-    ],
+    static: path.join(__dirname, 'dist'),
+    port: 3003,
+    open: true,
+    historyApiFallback: true
   },
   module: {
     rules: [
       {
-        test: /\.html$/i,
+        test: /\.js$/,
         exclude: /node_modules/,
-        use: { loader: HTML_LOADER }
+        use: {
+          loader: 'babel-loader'
+        }
       },
       {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: { loader: BABEL_LOADER },
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          CSS_LOADER,
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.mp3$/,
-        use: [
-          {
-            loader: FILE_LOADER,
-            options: {
-              name: MP3_NAME,
-              outputPath: AUDIO_PATH,
-              publicPath: AUDIO_PATH,
-            },
-          },
-        ],
-      },
-    ],
-  },
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 }
-
-if (!IS_DEV) {
-  config.module.rules.push({
-    test: /\.m?js$/,
-    exclude: /node_modules/,
-    use: {
-      loader: STRING_REPLACE_LOADER,
-      options: {
-        search: SERVER_URL,
-        replace: '',
-      },
-    },
-  })
-}
-
-module.exports = config
